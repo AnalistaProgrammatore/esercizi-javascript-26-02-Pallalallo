7a80f360d3e1741333eece369dd36bd57708b389
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-03-03 16:40:04 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,28 @@
 /**
-* Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
+
+const arrayToList = function (x) {
+    let aList = null;
+    const y = (k, b) => x[x.length - b - 1]
+    const z = (k, b) => {
+        aList = {
+            value: k, rest: aList
+        }
+    }
+    x.map(y).map(z);
+    return aList;
+}
+
+//questo non funziona è una prova che ho fatto
+//supponendo di poter usare reduce su una lista,
+//gli altri esercizi di aList li farei uguali a 
+//quelli del 19/02
+const listToArray = function (a) {
+    b = [];
+    const y = (k, z) => { z = aList.value; aList = aList.rest; return z }
+    b = b.concat(a.reduce(y));
+    return b
+}
+
+
+console.log(arrayToList([1, 2, 3]))
 **/
