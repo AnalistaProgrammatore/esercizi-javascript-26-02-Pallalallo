d5d0bfd4b36fff1a4c869dcce48d3707c665e4b7
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-03-03 16:38:41 +0100

    Update every.js

--- every.js
+++ every.js
@@ -1,1 +1,15 @@
 
+let k = null;
+function everyReduce(x, y) {
+    return x.reduce(y, true)
+
+}
+function everyBase(x, y) {
+    for (let a of x) {
+        if (!(y(a))) return false;
+    }
+    return true
+}
+
+
+console.log(everyReduce([10, 2, 6, -12, 8, 2], (k, y) => { return (y % 2) === 0 && k }));
