a3b452c7eea05df916b9b13222828d3f362b3ce8
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-03-03 16:41:46 +0100

    Update some.js

--- some.js
+++ some.js
@@ -1,1 +1,17 @@
+let k = false;
+function someBase(x, y) {
+    let v = null
+    for (let a of x) {
+        if (y(a)) {
+            v = true;
+        }
+    }
+    if (v) return true;
+    else return false
+}
 
+function someReduce(x, y) {
+    return x.reduce(y, null)
+}
+
+console.log(someReduce([1, 3, 0, 1], (k, y) => { return (y > 2) || k }))
