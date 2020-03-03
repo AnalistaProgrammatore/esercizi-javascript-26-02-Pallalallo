027a49cdf10ca8ca2acb3bc9c96bfe4fa08e8cbd
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-03-03 16:41:06 +0100

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,1 +1,9 @@
-
+x = [1, 2, 3, 4];
+const reduceBase = function (x, f, init_value) {
+    for (let i of x) {
+        init_value = f(init_value, i)
+    }
+    return init_value
+}
+const f = (a, b) => a + b;
+console.log(reduceBase(x, f, 0))
