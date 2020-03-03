b07a1f0b713a005242c0d2225e92ebf9a14c92de
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-03-03 16:40:40 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,10 @@
 /**
-* Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
+const Range = function (start, stop, step) {
+    return Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
+
+}
+const RangeNamed = ({ start = 0, stop = 30, step = 3 } = {}) => { return Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)) }
+
+let = start = 1, stop = 20, step = 2;
+console.log(RangeNamed({ start: 10, stop, step }))
 **/
